ab0a79237bd033f0a1bc3c280d0916e4850547f9
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-07-26 18:11:50 +0200

    adedd-props

---
+++ src/components/DiskCard.vue
@@ -0,0 +1,30 @@
+<template>
+   <div class="disk-card text-center p-2">
+      <img :src="image" :alt="name" class="image-fluid mb-3">
+      <h2>{{name}}</h2>
+      <div class="group">{{group}}</div>
+      <div class="year">{{year}}</div>
+   </div>
+</template>
+
+<script>
+  export default {
+    name:'DiskCard',
+    props: {
+      name: String,
+      image: String,
+      group: String,
+      year: String
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.disk-card{
+    width: 20%;
+}
+
+img{
+    height: 200px;
+}
+</style>
--- src/components/DisksSection.vue
+++ src/components/DisksSection.vue
@@ -1,14 +1,35 @@
 <template>
-  <section id="disks"></section>
+  <section id="disks">
+    <div class="disks-list gy-4 d-flex flex-wrap">
+        <div  
+        v-for="disk in disks" 
+        :key="disk.author">
+        <DiskCard 
+        :name="disk.name" 
+        :image="disk.poster"  
+        :group="disk.author" 
+        :year="disk.year" />
+        </div>
+    </div>
+  </section>
 </template>
 
 <script>
 import axios from 'axios';
+import DiskCard from './DiskCard.vue';
 export default{
-    name: 'DisksSection',
-    mounted(){
-        axios.get('https://flynn.boolean.careers/exercises/api/array/music').then(()=>{})
-    }
+    name: "DisksSection",
+    data() {
+        return {
+            disks: []
+        };
+    },
+    mounted() {
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res) => {
+            this.disks = res.data.response;
+        });
+    },
+    components: { DiskCard }
 }
 </script>
 
